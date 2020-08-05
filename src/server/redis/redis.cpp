779acae65598990fda75d1e66ec0926e1971bc89
bugken <yanzhenwei2011@163.com>
2020-08-05 07:02:02 -0700

    修正redis接口

--- src/server/redis/redis.cpp
+++ src/server/redis/redis.cpp
@@ -1,17 +1,21 @@
 #include "redis.hpp"
+#include <iostream>
+using namespace std;
 
 Redis::Redis():_publish_context(nullptr), _subscribe_context(nullptr)
 {
 }
+
 Redis::~Redis()
 {
     if (_publish_context != nullptr)
     {
-        _publish_context = nullptr;
+        redisFree(_publish_context);
     }
+
     if (_subscribe_context != nullptr)
     {
-        _subscribe_context = nullptr;
+        redisFree(_subscribe_context);
     }
 }
 //连接redis服务器
@@ -39,7 +43,7 @@ bool Redis::connect()
     });
     t.detach();
 
-    cout << "connect redis-server success." << endl;
+   cout << "connect redis-server success." << endl;
     return true;
 }
 //向redis指定通道publish发布消息
@@ -60,49 +64,49 @@ bool Redis::publish(int channel, string message)
 //向redis指定通道subscribe订阅消息
 bool Redis::subscribe(int channel)
 {
-    //SUBSCRIBE命令本身会造成线程阻塞等待通道里面发生消息，这里只做订阅通道，不接收通道消息
+//SUBSCRIBE命令本身会造成线程阻塞等待通道里面发生消息，这里只做订阅通道，不接收通道消息
     //通道消息的接收专门在observer_channel_message函数中的独立线程中进行
     //值负责发送命令，不阻塞接收redis server相应消息，否则和notifyMsg线程抢占响应资源
-    if (REDIS_ERR == redisAppendCommand(this->_subscribe_context, "SUBSCRIBE %d", channel));
+    if (REDIS_ERR == redisAppendCommand(this->_subscribe_context, "SUBSCRIBE %d", channel))
     {
         cerr << "subscribe command failed." << endl;
         return false;
     }
 
-    //redisBufferWrite可以循环发送缓冲区，知道缓冲区数据发送完毕(done被重置为1)
+     //redisBufferWrite可以循环发送缓冲区，知道缓冲区数据发送完毕(done被重置为1)
     int done = 0;
     while (!done)
     {
-       if (REDIS_ERR == redisBufferWrite(this->_subscribe_context, &done))
-       {
-           cerr << "subcribe command failed." << endl;
-           return false;
-       }
+        if (REDIS_ERR == redisBufferWrite(this->_subscribe_context, &done))
+        {
+            cerr << "subcribe command failed." << endl;
+            return false;
+        }
     }
 
-    return true;    
+    return true;
 }
 //向redis指定通道unsubscribe取消订阅消息
 bool Redis::unsubscribe(int channel)
 {
-    if (REDIS_ERR == redisAppendCommand(this->_subscribe_context, "UNSUBSCRIBE %d", channel));
+    if (REDIS_ERR == redisAppendCommand(this->_subscribe_context, "UNSUBSCRIBE %d", channel))
     {
         cerr << "unsubscribe command failed." << endl;
         return false;
     }
 
-    //redisBufferWrite可以循环发送缓冲区，知道缓冲区数据发送完毕(done被重置为1)
+//redisBufferWrite可以循环发送缓冲区，知道缓冲区数据发送完毕(done被重置为1)
     int done = 0;
     while (!done)
     {
-       if (REDIS_ERR == redisBufferWrite(this->_subscribe_context, &done))
-       {
-           cerr << "unsubcribe command failed." << endl;
-           return false;
-       }
+        if (REDIS_ERR == redisBufferWrite(this->_subscribe_context, &done))
+        {
+            cerr << "unsubcribe command failed." << endl;
+            return false;
+        }
     }
 
-    return true; 
+    return true;
 }
 //在独立线程中接收订阅通道中的消息
 void Redis::observer_channel_message()
@@ -118,7 +122,7 @@ void Redis::observer_channel_message()
         }
         freeReplyObject(reply);
     }
-    cerr << ">>>>>>>>>>>>>>>>> observer_channel_message quit <<<<<<<<<<<<<<<<<<<<<" << endl;
+cerr << ">>>>>>>>>>>>>>>>> observer_channel_message quit <<<<<<<<<<<<<<<<<<<<<" << endl;
 }
 //初始化向业务层上报通道消息的回调对象
 void Redis::init_notify_handler(function<void(int, string)> fn)
