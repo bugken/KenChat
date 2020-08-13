20aaa05ae06cf4358fc192a1c904e18132cf1390
awei <awei@awei.com>
2020-08-14 00:23:19 +0800

    实现登录逻辑并处理登录异常，返回数据待解析

--- chatwebclient/ChatClient/Login.aspx.cs
+++ chatwebclient/ChatClient/Login.aspx.cs
@@ -11,83 +11,119 @@ using System.Text;
 
 namespace ChatClient
 {
+    public class LoginInfo
+    {
+        public int msgid { get; set; }
+        public int id { get; set; }
+        public string password { get; set; }
+    }
     public partial class Login : System.Web.UI.Page
     {
         private string URLs = "http://192.168.107.128:8000";
         private string JsonStr = "{\"msgid\":1,\"id\":1, \"password\":\"123456\"}";
-        private string SendHttpRequest()
+        private JavaScriptSerializer Jss;
+        private LoginInfo loginInfo;
+        private HttpWebRequest httpWebRequest;
+        public Login()
         {
-            System.Diagnostics.Debug.WriteLine("SendHttpRequest");
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(URLs);
-            httpWebRequest.ContentType = "application/json";
+            System.Diagnostics.Debug.WriteLine("Login");
+            loginInfo = new LoginInfo();
+            loginInfo.msgid = 1;
+            loginInfo.id = 1;
+            loginInfo.password = "123456";
+            Jss = new JavaScriptSerializer();
+
+            httpWebRequest = (HttpWebRequest)WebRequest.Create(URLs);
             httpWebRequest.Method = "POST";
+            httpWebRequest.ContentType = "application/json";
             httpWebRequest.UserAgent = null;
             httpWebRequest.Timeout = 1000;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
- 
+        }
+        ~Login()
+        {
+            System.Diagnostics.Debug.WriteLine("~Login");
+        }
+        private bool LoginByHttpWebRequestAndStream(ref string DataBuff)
+        {
+            System.Diagnostics.Debug.WriteLine("LoginByHttpWebRequestAndStream");
+
+            bool Result = true;
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
-                string json = new JavaScriptSerializer().Serialize(new
-                {
-                    msgid = 1,
-                    id = 1,
-                    password = "123456"
-                });
+                string json = Jss.Serialize(loginInfo);
                 streamWriter.Write(json);
                 streamWriter.Flush();
                 streamWriter.Close();
             }
 
-            string retString;
-            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            try
             {
-                retString = streamReader.ReadToEnd();
+                HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    DataBuff = streamReader.ReadToEnd();
+                }
             }
-            return retString;
-        }
-        private string SendHttpRequest2()
-        {
-            System.Diagnostics.Debug.WriteLine("SendHttpRequest2");
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URLs);
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-            httpWebRequest.UserAgent = null;
-            httpWebRequest.Timeout = 1000;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-            string json = new JavaScriptSerializer().Serialize(new
+            catch (Exception ex)
             {
-                msgid = 1,
-                id = 1,
-                password = "123456"
-            });
+                Response.Write("<script>alert('(" + ex.GetType().Name + ")" + ex.Message + "')</script>");
+                DataBuff = ex.Message;
+                Result = false;
+            }
+            return Result;
+        }
+        private bool LoginByHttpWebRequestAndWrite(ref string DataBuff)
+        {
+            System.Diagnostics.Debug.WriteLine("LoginByHttpWebRequestAndWrite");
+
+            bool Result = true;
+            string json = Jss.Serialize(loginInfo);
             byte[] postBytes = Encoding.ASCII.GetBytes(json);
             httpWebRequest.ContentLength = postBytes.Length;
-            using (Stream reqStream = httpWebRequest.GetRequestStream())
+
+            try
             {
-                reqStream.Write(postBytes, 0, postBytes.Length);
+                using (Stream reqStream = httpWebRequest.GetRequestStream())
+                {
+                    reqStream.Write(postBytes, 0, postBytes.Length);
+                }
+                HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    DataBuff = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('(" + ex.GetType().Name + ")" + ex.Message + "')</script>");
+                DataBuff = ex.Message;
+                Result = false;
             }
 
-            string retString;
-            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                retString = streamReader.ReadToEnd();
-            }
-            return retString;
+            return Result;
         }
-        private string SendHttpRequest3()
+        private bool LoginByWebClient(ref string DataBuff)
         {
-            System.Diagnostics.Debug.WriteLine("SendHttpRequest3");
-            byte[] jsonData = Encoding.UTF8.GetBytes(JsonStr);
+            System.Diagnostics.Debug.WriteLine("LoginByWebClient");
 
+            bool Result = true;
             WebClient client = new WebClient();
+            byte[] jsonData = Encoding.UTF8.GetBytes(JsonStr);
             client.Headers.Add("Content-Type", "application/json"); //采取POST方式必须加的header
             client.Headers.Add("ContentLength", jsonData.Length.ToString());
-            byte[] responseData = client.UploadData(URLs, "POST", jsonData); //得到返回字符流
-            string retString = Encoding.UTF8.GetString(responseData); //解码
+            try
+            {
+                byte[] responseData = client.UploadData(URLs, "POST", jsonData); //得到返回字符流
+                DataBuff = Encoding.UTF8.GetString(responseData); //解码
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('(" + ex.GetType().Name + ")" + ex.Message + "')</script>");
+                DataBuff = ex.Message;
+                Result = false;
+            }
 
-            return retString;
+            return Result;
         }
 
         protected void Page_Load(object sender, EventArgs e)
@@ -97,12 +133,23 @@ namespace ChatClient
         protected void BtnLoginClick(object sender, EventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("BtnLoginClick");
-            Response.Write("BtnLoginClick");
-            SendHttpRequest3();
+
+            string DataBuff = "";
+            //if (LoginByWebClient(ref DataBuff))
+            if (LoginByHttpWebRequestAndWrite(ref DataBuff))
+            //if (LoginByHttpWebRequestAndStream(ref DataBuff))
+            {
+                Response.Write(DataBuff);
+                Response.Write("BtnLoginClick Success");
+            }
+            else 
+            {
+                Response.Write("BtnLoginClick Failed");
+            }
         }
         protected void LinkBtnToRegisterClick(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("BtnLoginClick");
+            System.Diagnostics.Debug.WriteLine("LinkBtnToRegisterClick");
             Response.Write("LinkBtnToRegisterClick");
             Response.Redirect("Register.aspx");
         }
