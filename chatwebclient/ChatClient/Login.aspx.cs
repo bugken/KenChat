c423b5950a850fd9ec3282996a9861813c1e64c3
awei <awei@awei.com>
2020-08-13 20:41:04 +0800

    增加web客户端模块，web客户端含有待完善的登录以及注册两个简单页面

---
+++ chatwebclient/ChatClient/Login.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Text;
+
+namespace ChatClient
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private string URLs = "http://192.168.107.128:8000";
+        private string JsonStr = "{\"msgid\":1,\"id\":1, \"password\":\"123456\"}";
+        private string SendHttpRequest()
+        {
+            System.Diagnostics.Debug.WriteLine("SendHttpRequest");
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(URLs);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.UserAgent = null;
+            httpWebRequest.Timeout = 1000;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+ 
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = new JavaScriptSerializer().Serialize(new
+                {
+                    msgid = 1,
+                    id = 1,
+                    password = "123456"
+                });
+                System.Diagnostics.Debug.WriteLine(json);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            string retString;
+            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                retString = streamReader.ReadToEnd();
+            }
+            System.Diagnostics.Debug.WriteLine(retString);
+            return retString;
+        }
+        private string SendHttpRequest2()
+        {
+            System.Diagnostics.Debug.WriteLine("SendHttpRequest2");
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URLs);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.UserAgent = null;
+            httpWebRequest.Timeout = 1000;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            string json = new JavaScriptSerializer().Serialize(new
+            {
+                msgid = 1,
+                id = 1,
+                password = "123456"
+            });
+            byte[] postBytes = Encoding.ASCII.GetBytes(json);
+            httpWebRequest.ContentLength = postBytes.Length;
+            using (Stream reqStream = httpWebRequest.GetRequestStream())
+            {
+                reqStream.Write(postBytes, 0, postBytes.Length);
+            }
+
+            string retString;
+            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                retString = streamReader.ReadToEnd();
+            }
+            System.Diagnostics.Debug.WriteLine(retString);
+            return retString;
+        }
+        private string SendHttpRequest3()
+        {
+            System.Diagnostics.Debug.WriteLine("SendHttpRequest3");
+            byte[] jsonData = Encoding.UTF8.GetBytes(JsonStr);
+
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json"); //采取POST方式必须加的header
+            client.Headers.Add("ContentLength", jsonData.Length.ToString());
+            byte[] responseData = client.UploadData(URLs, "POST", jsonData); //得到返回字符流
+            string retString = Encoding.UTF8.GetString(responseData); //解码
+
+            return retString;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("Button1_Click");
+            System.Diagnostics.Debug.WriteLine("111200000011111");
+            SendHttpRequest3();
+        }
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+    }
+}
---
+++ chatwebclient/ChatClient/Register.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChatClient
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        private bool UserNameIselgal = false;
+        private bool PsdIselgal = false;
+        private bool CanRegister = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void linkToLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Write("btnRegister_Click!");
+        }
+        protected void CustomValidator1_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
+        {
+            if (rUserNameText.Text.Equals("用户名"))
+            {
+                CustomValidator1.ErrorMessage = "*用户名为空";
+                args.IsValid = false;
+            }
+            else if (System.Text.RegularExpressions.Regex.IsMatch(rUserNameText.Text, "^[0-9a-zA-Z]+$") &&
+                  rUserNameText.Text.Length > 5 && rUserNameText.Text.Length < 11)
+            {
+                args.IsValid = true;
+                UserNameIselgal = true;
+            }
+            else
+            {
+                CustomValidator1.ErrorMessage = "*用户名由6~10位数字和字母构成";
+                args.IsValid = false;
+            }
+        }
+        protected void CustomValidator2_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
+        {
+            if (rPsdText.Text.Equals("密码"))
+            {
+                CustomValidator2.ErrorMessage = "*密码为空";
+                args.IsValid = false;
+            }
+            else if (System.Text.RegularExpressions.Regex.IsMatch(rPsdText.Text, "^[0-9a-zA-Z]+$") &&
+              rPsdText.Text.Length > 4)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                CustomValidator2.ErrorMessage = "*密码由全数字和字母构成且不少于5位";
+                args.IsValid = false;
+            }
+        }
+        protected void CustomValidator3_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
+        {
+            if (rrPsdText.Text.Equals("") || rrPsdText.Text.Equals("确认密码"))
+            {
+                args.IsValid = false;
+                CustomValidator3.ErrorMessage = "*确认密码为空";
+            }
+            else if (!rrPsdText.Text.Equals(rPsdText.Text))
+            {
+                args.IsValid = false;
+                CustomValidator3.ErrorMessage = "*两次密码不一致";
+            }
+            else
+            {
+                PsdIselgal = true;
+                args.IsValid = true;
+            }
+        }
+    }
+}
