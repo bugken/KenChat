81341304103bcaeacca3f6b25edcfc87b0b55653
awei <awei@awei.com>
2020-08-15 00:58:11 +0800

    实现注册功能

--- chatwebclient/ChatClient/Register.aspx.cs
+++ chatwebclient/ChatClient/Register.aspx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ChatClient
 {
+    public class RegisterInfo
+    {
+        public int msgid { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+    class RegisterReplyInfo
+    {
+        public int errno { get; set; }
+        public string errmsg { get; set; }
+        public int msgid { get; set; }
+        public int id { get; set; }
+    }
     public partial class Register : System.Web.UI.Page
     {
         private bool UserNameIselgal = false;
         private bool PsdIselgal = false;
         private bool CanRegister = false;
+        private string URLs = "http://192.168.107.128:8000";
+        private JavaScriptSerializer JsonParserJss;
+        private RegisterInfo resisterInfo;
+
+        public Register()
+        {
+            System.Diagnostics.Debug.WriteLine("Register");
+            resisterInfo = new RegisterInfo();
+            resisterInfo.msgid = 3;
+
+            JsonParserJss = new JavaScriptSerializer();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,13 +51,54 @@ namespace ChatClient
         {
             Response.Redirect("Login.aspx");
         }
+        private bool RegisterByWebClient(ref string DataBuff)
+        {
+            System.Diagnostics.Debug.WriteLine("RegisterByWebClient");
+
+            bool Result = true;
+            WebClient client = new WebClient();
+            byte[] jsonData = Encoding.UTF8.GetBytes(JsonParserJss.Serialize(resisterInfo));
+            client.Headers.Add("Content-Type", "application/json"); //采取POST方式必须加的header
+            client.Headers.Add("ContentLength", jsonData.Length.ToString());
+            try
+            {
+                byte[] responseData = client.UploadData(URLs, "POST", jsonData); //得到返回字符流
+                DataBuff = Encoding.UTF8.GetString(responseData); //解码
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('(" + ex.GetType().Name + ")" + ex.Message + "')</script>");
+                DataBuff = ex.Message;
+                Result = false;
+            }
+
+            return Result;
+        }
         protected void BtnRegisterClick(object sender, EventArgs e)
         {
-            Response.Write("btnRegister_Click!");
-            /*读取输入信息*/
-            /*注册信息*/
-            /*解析Json*/
-            /*注册成功返回UserID并且转到Lobby*/
+            System.Diagnostics.Debug.WriteLine("BtnRegisterClick");
+            resisterInfo.name = rUserNameText.Text;
+            resisterInfo.password = rPsdText.Text;
+
+            string RegisterBuff = "";
+            if (RegisterByWebClient(ref RegisterBuff))
+            {
+                System.Diagnostics.Debug.WriteLine(RegisterBuff);
+                //RegisterReplyInfo registerReplyData = JsonParserJss.Deserialize<RegisterReplyInfo>(RegisterBuff);
+                RegisterReplyInfo registerReplyData = JsonConvert.DeserializeObject<RegisterReplyInfo>(RegisterBuff);
+                if (registerReplyData.errno == 0)
+                {
+                    Response.Redirect("ChatLobby.aspx", false);
+                }
+                else
+                {
+                    Response.Write("<script>alert('" + registerReplyData.errmsg + "')</script>");
+                }
+            }
+            else 
+            {
+                Response.Write("BtnRegisterClick Failed");
+            }
         }
         protected void CustomValidator1_ServerValidate(object source, System.Web.UI.WebControls.ServerValidateEventArgs args)
         {
